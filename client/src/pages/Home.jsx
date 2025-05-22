b5608b4bec97acfacbf832fb820b2ffdbdad296c
c4dr-me <142721314+c4dr-me@users.noreply.github.com>
2025-05-22 15:16:55 +0530

    Auto Commit: $(date +'%Y-%m-%d %H:%M:%S')

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import Navbar from '../components/Navbar'
 import img from '../assets/header_back2.jpg'
-import homeImg from '../assets/home.jpg'
+import homeImg from '../assets/home.png'
 function Home() {
   return (
     <div className='bg-cover bg-center h-screen ' style={{backgroundImage: `url(${img})`}}>
