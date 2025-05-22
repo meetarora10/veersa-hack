2f7234237ec1f59f1045d98d63344cdae14603ec
c4dr-me <142721314+c4dr-me@users.noreply.github.com>
2025-05-22 16:00:41 +0530

    Auto Commit: $(date +'%Y-%m-%d %H:%M:%S')

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,13 +2,15 @@ import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./components/Contact";
+import Doctors from "./components/Doctors";
 
 function App(){
     return (
         <div>
             <Home />
             <About />
-            <Contact/>
+            <Contact />
+            <Doctors/>
         </div>
     );
 }
