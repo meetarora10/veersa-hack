d041e0ce737003f2fa5f5adc7c16c908c6513b48
meetarora <meet.arora2403@gmail.com>
2025-05-26 00:27:03 +0530

    minor

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,6 +1,8 @@
-import { FaGithub, FaDiscord, FaEnvelope, FaUserMd } from 'react-icons/fa';
-
+import { FaGithub, FaDiscord, FaEnvelope } from 'react-icons/fa';
+import { Link } from 'react-scroll'
+import { useNavigate } from 'react-router-dom';
 export default function Footer() {
+  const navigate = useNavigate();
   return (
     <footer className="bg-[#0e0e12] text-gray-200 pt-10 pb-6 px-6 sm:px-16">
       <div className="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-3 gap-10">
@@ -20,18 +22,17 @@ export default function Footer() {
         <div>
           <h3 className="text-lg font-semibold mb-3 text-white">Shop & More</h3>
           <ul className="space-y-2 text-sm">
-            <li className="hover:underline cursor-pointer"> Buy Medicines</li>
-            <li className="hover:underline cursor-pointer"> Disease Prediction</li>
-            <li className="hover:underline cursor-pointer"> Book an Appointment</li>
+
+            <li className="hover:underline cursor-pointer" onClick={() => navigate('/register')}> Book an Appointment</li>
           </ul>
         </div>
         <div>
           <h3 className="text-lg font-semibold mb-3 text-white">Ours</h3>
           <ul className="space-y-2 text-sm">
-            <li className="hover:underline cursor-pointer"> About Us</li>
-            <li className="hover:underline cursor-pointer"> Contact Us</li>
-            <li className="hover:underline cursor-pointer"> Rate Us</li>
-            <li className="hover:underline cursor-pointer"> Privacy Policies</li>
+            <li className="hover:underline cursor-pointer"><Link to='home' spy={true} smooth={true} duration={500}> Home</Link></li>
+            <li className="hover:underline cursor-pointer"><Link to='about' spy={true} smooth={true} duration={500}> About Us</Link></li>
+            <li className="hover:underline cursor-pointer"><Link to='doctor' spy={true} smooth={true} duration={500}> Doctors</Link></li>
+            <li className="hover:underline cursor-pointer"><Link to='contact' spy={true} smooth={true} duration={500}> Contact Us</Link></li>
           </ul>
         </div>
       </div>
@@ -40,7 +41,7 @@ export default function Footer() {
         
         {/* Social Icons */}
         <div className="flex gap-5 mt-4 md:mt-0 text-white">
-          <a href="#" className="hover:text-blue-500">
+          <a href="https://github.com/meetarora10/veersa-hack" className="hover:text-blue-500">
             <FaGithub size={20} />
           </a>
           <a href="#" className="hover:text-indigo-400">
@@ -48,9 +49,6 @@ export default function Footer() {
           </a>
           <a href="#" className="hover:text-yellow-400">
             <FaEnvelope size={20} />
-          </a>
-          <a href="#" className="hover:text-green-400">
-            <FaUserMd size={20} />
           </a>
         </div>
       </div>
