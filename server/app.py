38506877b8888aca395482bd96db4e6a0a2406d0
c4dr-me <142721314+c4dr-me@users.noreply.github.com>
2025-05-25 23:43:10 +0530

    host

--- server/app.py
+++ server/app.py
@@ -78,5 +78,6 @@ def start_transcription(room_name):
         return jsonify({'error': str(e)}), 500    
     
 if __name__ == '__main__':
-    chat_server.run(app, debug=True, port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    chat_server.run(app, host='0.0.0.0', port=port)
 
