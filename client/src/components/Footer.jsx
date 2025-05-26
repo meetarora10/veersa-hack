e50897a16fd9a5d1e37cb9a252918c2ee0e65a50
meetarora <meet.arora2403@gmail.com>
2025-05-26 07:33:30 +0530

    fix-2

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -11,7 +11,7 @@ export default function Footer() {
           <div className="flex items-center gap-2 mb-4">
             <span className="text-red-500 text-3xl">❤️</span>
             <h2 className="text-2xl font-bold">
-              <span className="italic text-indigo-300">Tea</span>
+              <span className="italic text-teal-400/70">Tea</span>
               <span className="text-blue-400 font-bold">lth</span>
             </h2>
           </div>
@@ -38,7 +38,7 @@ export default function Footer() {
       </div>
       <div className="border-t border-gray-800 mt-10 pt-6 flex flex-col md:flex-row justify-between items-center text-sm">
         <p>© 2025 Tealth. All rights reserved.</p>
-        
+
         {/* Social Icons */}
         <div className="flex gap-5 mt-4 md:mt-0 text-white">
           <a href="https://github.com/meetarora10/veersa-hack" className="hover:text-blue-500">
